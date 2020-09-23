a53dea7c09ac1242d3ed98f502897ea31b19806f
qcaiiocf <qcaiiocf>
2020-09-23 10:21:36 +0800

    Signed-off-by: qcaiiocf <qcaiiocf>

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,18 +2,8 @@ export default {
     getBaseUrl() {
         let baseURL = '';
         if (process.env.NODE_ENV === 'development') {
-        baseURL = "http://chatdev.h5more.com";
+            baseURL = "http://xxx.xxx.com";
         }
-        // if (process.env.VUE_APP_IDENTITY && process.env.VUE_APP_IDENTITY === 'PRODUCT') {
-        //     url = 'https://pm.h5more.com';
-        // } else if (process.env.VUE_APP_IDENTITY && process.env.VUE_APP_IDENTITY === 'TEST') {
-        //     url = 'http://chatdev.h5more.com';
-        // } else if (process.env.VUE_APP_IDENTITY && process.env.VUE_APP_IDENTITY === 'LOCAL') {
-        //     url = 'http://chat.dev.cn';
-        // }
-        // if (process.env.NODE_ENV !== 'development' && process.env.VUE_APP_IDENTITY === 'production') {
-        //     url = 'https://pm.h5more.com';
-        // }
         return baseURL;
     }
 }
